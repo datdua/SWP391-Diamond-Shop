42fbe54c456be10e68ca366a53f4d8c802d25bee
phuccoder <thienphuc8102003@gmail.com>
2024-05-08 20:37:22 +0700

    Cấu hình và API test

---
+++ Back-End/diamondstore/src/main/java/com/example/diamondstore/model/User.java
@@ -0,0 +1,60 @@
+package com.example.diamondstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "diamond_user")
+public class User {
+    
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "id")
+     private Long id;
+ 
+     @Column(name = "username")
+     private String username;
+ 
+     @Column(name = "password")
+     private String password;
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+     
+
+}
