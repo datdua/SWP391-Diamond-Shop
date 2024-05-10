f18f9a383e91cfc32b6d13bd2f55699992e90d09
ThDosu <phamlethanhdung04@gmail.com>
2024-05-10 14:28:22 +0700

    demo test api 1

---
+++ BE-ThànhDũng/diamondshopdemo/src/main/java/edu/fpt/diamondshopdemo/DiamondshopdemoApplication.java
@@ -0,0 +1,13 @@
+package edu.fpt.diamondshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondshopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondshopdemoApplication.class, args);
+	}
+
+}
---
+++ BE-ThànhDũng/diamondshopdemo/src/test/java/edu/fpt/diamondshopdemo/DiamondshopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fpt.diamondshopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondshopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
