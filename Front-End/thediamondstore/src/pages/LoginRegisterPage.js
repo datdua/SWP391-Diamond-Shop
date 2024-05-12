e739c512d7c3a82ec953d3445bd1fa5fda82dc13
AnPT173 <phanthienan01072003@gmail.com>
2024-05-12 10:20:06 +0700

    build front end

---
+++ Front-End/thediamondstore/src/pages/LoginRegisterPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginRegister from "../components/LoginRegister";
+function LoginRegisterPage (){
+    return(
+        <LoginRegister/>
+    )
+}
+export default LoginRegisterPage
