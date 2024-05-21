6d24d091f1e4d888ae46cd563c147749819bbb77
ThDosu <phamlethanhdung04@gmail.com>
2024-05-21 13:04:45 +0700

    null

--- Test/thediamondstore/src/main/java/com/example/diamondstore/repository/DiamondRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.diamondstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.diamondstore.model.Diamond;
-
-public interface DiamondRepository extends JpaRepository<Diamond, Integer>{
-    
-}
--- Test/thediamondstore/src/main/java/com/example/diamondstore/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.diamondstore.repository;
-
-import com.example.diamondstore.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
