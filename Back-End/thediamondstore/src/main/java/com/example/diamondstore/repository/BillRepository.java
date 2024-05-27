fce1a893c5997f97c1f736875400c1f247d5b1e9
ThDosu <phamlethanhdung04@gmail.com>
2024-05-27 11:54:19 +0700

    delete bill

--- Back-End/thediamondstore/src/main/java/com/example/diamondstore/repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.diamondstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.diamondstore.model.Bill;
-
-public interface BillRepository extends JpaRepository<Bill, Integer>{
-    
-}
