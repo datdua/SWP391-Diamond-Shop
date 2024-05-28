9bdf5d2595c5cddd3eecbfd9e81ba4c8777b886d
Nguyễn Tấn Đạt <toimqn09@gmail.com>
2024-05-28 08:45:06 +0700

    fix-trangchu2-backtotop

--- Front-End/thediamondstore/src/App.js
+++ Front-End/thediamondstore/src/App.js
@@ -1,34 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import AboutPage from './pages/AboutPage';
-import HomePage from './pages/HomePage';
-import CartPage from './pages/CartPage';
-import LoginRegisterPage from './pages/LoginRegisterPage';
-import ProductPage from './pages/ProductPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import MyAccountPage from './pages/MyAccountPage';
-import CheckoutPage from './pages/CheckoutPage';
-import WishlistPage from './pages/WishlistPage';
-import PortfolioPage from './pages/PortfolioPage';
-import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/About/AboutPage';
+import HomePage from './pages/Home/HomePage';
+import CartPage from './pages/Cart/CartPage';
+import LoginRegisterPage from './pages/LoginRegister/LoginRegisterPage';
+import ProductPage from './pages/Product/ProductPage';
+import ProductDetailPage from './pages/ProductDetail/ProductDetailPage';
+import MyAccountPage from './pages/MyAccount/MyAccountPage';
+import CheckoutPage from './pages/Checkout/CheckoutPage';
+import WishlistPage from './pages/Wishlist/WishlistPage';
+import PortfolioPage from './pages/Portfolio/PortfolioPage';
+import ContactPage from './pages/Contact/ContactPage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import BackToTop from './components/BackToTop';
+
 
 function App() {
   return (
-    <Router>
+    <>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/about' element={<AboutPage/>}/>
-      <Route path='/cart' element={<CartPage/>}/>
-      <Route path='/login' element={<LoginRegisterPage/>}/>
-      <Route path='/product' element={<ProductPage/>}/>
-      <Route path='/product-detail' element={<ProductDetailPage/>}/>
-      <Route path='/account' element={<MyAccountPage/>}/>
-      <Route path='/checkout' element={<CheckoutPage/>}/>
-      <Route path='/wishlist' element={<WishlistPage/>}/>
-      <Route path='portfolio' element={<PortfolioPage/>}/>
-      <Route path='contact' element={<ContactPage/>}/>
+        <Route path="/" element={<Navigate to="/trangchu" replace />} />
+        <Route path='/dangnhap' element={<><Header/> <LoginRegisterPage/> <Footer/></>}/>
+        <Route path='/trangchu' element={<><Header/> <HomePage/> <BackToTop/> <Footer/></>}/>
+        <Route path='/gioithieu' element={<><Header/> <AboutPage/> <Footer/></>}/>
+        <Route path='/lienhe' element={<><Header/> <ContactPage/> <Footer/></>}/>
+        <Route path='/sanpham' element={<><Header/> <ProductPage/> <BackToTop/> <Footer/></>}/>
+
+        <Route path='/cart' element={<CartPage/>}/>
+        <Route path="/product-detail/:jewelryId" element={<ProductDetailPage />} />
+        <Route path='/account' element={<MyAccountPage/>}/>
+        <Route path='/checkout' element={<CheckoutPage/>}/>
+        <Route path='/wishlist' element={<WishlistPage/>}/>
+        <Route path='/portfolio' element={<PortfolioPage/>}/>
       </Routes>
-    </Router>
+    </>
   );
 }
 
