67733aaf1418f11bdbf3f9e5be6cbe598074fb2e
ThDosu <phamlethanhdung04@gmail.com>
2024-05-30 09:30:20 +0700

    update getByAccountRole

--- Back-End/thediamondstore/src/main/java/com/example/diamondstore/controller/AccountController.java
+++ Back-End/thediamondstore/src/main/java/com/example/diamondstore/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.example.diamondstore.controller;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.domain.Page;
@@ -145,5 +146,15 @@ public class AccountController {
             return ResponseEntity.badRequest().body(Collections.singletonMap("message", "Không tìm thấy tài khoản"));
         }
         return ResponseEntity.ok(account);
+    }
+
+    //get account by role
+    @GetMapping("/getByRole/{role}")
+    public ResponseEntity<?> getByRole(@PathVariable String role) {
+        List<Account> accounts = accountRepository.findByRole(role);
+        if (accounts.isEmpty()) {
+            return ResponseEntity.badRequest().body(Collections.singletonMap("message", "Không tìm thấy tài khoản"));
+        }
+        return ResponseEntity.ok(accounts);
     }
 }
--- Back-End/thediamondstore/src/main/java/com/example/diamondstore/repository/AccountRepository.java
+++ Back-End/thediamondstore/src/main/java/com/example/diamondstore/repository/AccountRepository.java
@@ -1,5 +1,7 @@
 package com.example.diamondstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.diamondstore.model.Account;
@@ -12,4 +14,6 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     Account findByEmail(String email);
 
     Account findByAccountID(Integer accountID);
+
+    List<Account> findByRole(String role);
 }
