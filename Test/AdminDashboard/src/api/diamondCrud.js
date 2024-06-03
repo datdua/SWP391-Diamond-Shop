21a19fe3c374ec6966d3f612d60db80616e5e825
phuccoder <anhdeptraippt@gmail.com>
2024-06-03 16:44:35 +0700

    Update Account Put API and start using api from server in AdminDashboard

--- Test/AdminDashboard/src/api/diamondCrud.js
+++ Test/AdminDashboard/src/api/diamondCrud.js
@@ -1,82 +1,103 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function getAllDiamond() {
-    const response = await axios.get('http://localhost:8080/api/diamonds');
-    if (response.status !== 200) {
-        throw new Error('Failed to fetch diamond data');
-    }
-    return response.data;
+  const response = await axios.get(
+    "https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/diamonds"
+  );
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch diamond data");
+  }
+  return response.data;
 }
 
 export async function getDiamondByID(diamondID) {
-    try {
-        const response = await axios.get(`http://localhost:8080/api/diamonds/${diamondID}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to fetch diamond by ID');
-    }
+  try {
+    const response = await axios.get(
+      `https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/diamonds/${diamondID}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch diamond by ID");
+  }
 }
 export async function getPage(page = 1, size = 9) {
-    try {
-        const response = await axios.get(`http://localhost:8080/api/diamonds/paged?page=${page}&size=${size}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to fetch diamond by page');
-    }
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/api/diamonds/paged?page=${page}&size=${size}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch diamond by page");
+  }
 }
 export async function searchJewelryByGender(gender) {
-    try {
-        const response = await axios.get(`http://localhost:8080/api/jewelry/search/filter?gender=%20${encodeURIComponent(gender)}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to filter jewelry by gender');
-    }
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/api/jewelry/search/filter?gender=%20${encodeURIComponent(
+        gender
+      )}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to filter jewelry by gender");
+  }
 }
 
 export async function createDiamond(diamond) {
-    try {
-        const response = await axios.post('http://localhost:8080/api/diamonds/create', diamond);
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to create jewelry');
-    }
+  try {
+    const response = await axios.post(
+      "https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/diamonds/create",
+      diamond
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to create jewelry");
+  }
 }
 
 export async function updateDiamond(diamondID, diamond) {
-    try {
-        const response = await axios.put(`http://localhost:8080/api/diamonds/update/${diamondID}`, diamond);
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to update Diamond');
-    }
+  try {
+    const response = await axios.put(
+      `https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/diamonds/update/${diamondID}`,
+      diamond
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to update Diamond");
+  }
 }
 
 export async function deleteDiamond(diamondID) {
-    try {
-        const response = await axios.delete(`http://localhost:8080/api/diamonds/delete/${diamondID}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to delete diamond');
-    }
+  try {
+    const response = await axios.delete(
+      `https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/diamonds/delete/${diamondID}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to delete diamond");
+  }
 }
 
 export async function getCertificateImage(certificationID) {
-    try {
-      const response = await axios.get(`http://localhost:8080/api/certificates/get/certificateImg/${certificationID}`);
-      console.log('API Response:', response.data); // Debug line
-      return response.data.certificateImage; // Correctly extract the certificateImage URL
-    } catch (error) {
-      throw new Error('Failed to fetch diamond certificate image');
-    }
+  try {
+    const response = await axios.get(
+      `https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/certificates/get/certificateImg/${certificationID}`
+    );
+    console.log("API Response:", response.data); // Debug line
+    return response.data.certificateImage; // Correctly extract the certificateImage URL
+  } catch (error) {
+    throw new Error("Failed to fetch diamond certificate image");
   }
+}
 
 export async function getWarrantityImage(warrantyID) {
-    try {
-      const response = await axios.get(`http://localhost:8080/api/warranties/get/warrantyImg/${warrantyID}`);
-      console.log('API Response:', response.data); // Debug line
-      return response.data.warrantyImage; // Correctly extract the warrantityImage URL
-    } catch (error) {
-      throw new Error('Failed to fetch diamond warranty image');
-    }
+  try {
+    const response = await axios.get(
+      `https://diamondstore.lemonhill-6b585cc3.eastasia.azurecontainerapps.io/api/warranties/get/warrantyImg/${warrantyID}`
+    );
+    console.log("API Response:", response.data); // Debug line
+    return response.data.warrantyImage; // Correctly extract the warrantityImage URL
+  } catch (error) {
+    throw new Error("Failed to fetch diamond warranty image");
   }
-  
+}
