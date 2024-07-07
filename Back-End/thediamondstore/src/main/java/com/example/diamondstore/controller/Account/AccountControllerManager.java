6a0d2214d589c3c4647ae4ccce8710f33c30e316
ThDosu <phamlethanhdung04@gmail.com>
2024-07-07 15:30:44 +0700

    update: Account in Controller

--- Back-End/thediamondstore/src/main/java/com/example/diamondstore/controller/Account/AccountControllerManager.java
+++ Back-End/thediamondstore/src/main/java/com/example/diamondstore/controller/Account/AccountControllerManager.java
@@ -1,23 +1,15 @@
 package com.example.diamondstore.controller.Account;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.diamondstore.model.Account;
-import com.example.diamondstore.repository.AccountRepository;
 import com.example.diamondstore.request.AccountRequest;
 import com.example.diamondstore.service.AccountService;
 
@@ -26,26 +18,10 @@ import com.example.diamondstore.service.AccountService;
 @RequestMapping("/api/accounts/manager")
 public class AccountControllerManager {
 
-    private final AccountRepository accountRepository;
     private final AccountService accountService;
 
-    public AccountControllerManager(AccountRepository accountRepository, AccountService accountService) {
-        this.accountRepository = accountRepository;
+    public AccountControllerManager(AccountService accountService) {
         this.accountService = accountService;
-    }
-
-    @GetMapping("/accounts")
-    public ResponseEntity<Iterable<Account>> getAccounts_Manager() {
-        return ResponseEntity.ok(accountRepository.findAll());
-    }
-
-    @GetMapping("/{accountName}")
-    public ResponseEntity<Account> getByAccountName_Manager(@PathVariable String accountName) {
-        Account account = accountRepository.findByAccountName(accountName);
-        if (account == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(account);
     }
 
     @PutMapping(value = "/update/{accountID}", produces = "application/json;charset=UTF-8")
@@ -56,40 +32,5 @@ public class AccountControllerManager {
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(Collections.singletonMap("message", e.getMessage()));
         }
-    }
-
-    @GetMapping("/paged")
-    public ResponseEntity<Page<Account>> getAllAccountsPaged_Manager(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
-        Page<Account> pageAccount = accountRepository.findAll(pageable);
-        return ResponseEntity.ok(pageAccount);
-    }
-
-    @GetMapping("/get/{accountID}")
-    public ResponseEntity<?> getByAccountID_Manager(@PathVariable Integer accountID) {
-        Account account = accountRepository.findByAccountID(accountID);
-        if (account == null) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("message", "Không tìm thấy tài khoản"));
-        }
-        return ResponseEntity.ok(account);
-    }
-
-    @GetMapping("/getByEmail/{email}")
-    public ResponseEntity<?> getByAccountEmail_Manager(@PathVariable String email) {
-        Optional<Account> account = accountRepository.findByEmail(email);
-        if (account.isPresent()) {
-            return ResponseEntity.ok(account.get());
-        } else {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("message", "Không tìm thấy tài khoản"));
-        }
-    }
-
-    @GetMapping("/getByRole/{role}")
-    public ResponseEntity<?> getByAccountRole_Manager(@PathVariable String role) {
-        List<Account> accounts = accountRepository.findByRole(role);
-        if (accounts.isEmpty()) {
-            return ResponseEntity.badRequest().body(Collections.singletonMap("message", "Không tìm thấy tài khoản"));
-        }
-        return ResponseEntity.ok(accounts);
     }
 }
