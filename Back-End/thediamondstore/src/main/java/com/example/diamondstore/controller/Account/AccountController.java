cbdb024d4b9e199f149d1582f3afc377fbe5cafe
phuccoder <anhdeptraippt@gmail.com>
2024-07-19 16:30:05 +0700

    Change url /api/auth

--- Back-End/thediamondstore/src/main/java/com/example/diamondstore/controller/Account/AccountController.java
+++ Back-End/thediamondstore/src/main/java/com/example/diamondstore/controller/Account/AccountController.java
@@ -2,7 +2,6 @@ package com.example.diamondstore.controller.Account;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -11,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.diamondstore.model.Account;
 import com.example.diamondstore.repository.AccountRepository;
 import com.example.diamondstore.request.AccountRequest;
-import com.example.diamondstore.request.RegisterRequest;
 import com.example.diamondstore.service.AccountService;
 
 @RestController
@@ -91,7 +88,7 @@ public class AccountController {
     public List<Account> getAllAccountsExceptCustomer() {
         return accountService.getAllAccountsExcludingRoleCustomer();
     }
-    
+
     @PutMapping("/update/{accountID}")
     public ResponseEntity<Account> updateAccount_Admin(
             @PathVariable Integer accountID,
@@ -101,59 +98,5 @@ public class AccountController {
         String jwtToken = token.substring(7); // Remove "Bearer " prefix
         Account updatedAccount = accountService.updateAccountProfile(accountID, accountRequest, jwtToken);
         return ResponseEntity.ok(updatedAccount);
-    }
-
-    
-    @PostMapping(value = "/register", produces = "application/json;charset=UTF-8")
-    public ResponseEntity<Map<String, String>> register(@RequestBody RegisterRequest registerRequest) {
-        Map<String, String> message;
-        try {
-            message = accountService.register(registerRequest);
-            return ResponseEntity.ok().body(Map.of("message", message.get("message")));
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
-        }
-    }
-
-    @PutMapping(value = "/verify-account", produces = "application/json;charset=UTF-8")
-    public ResponseEntity<Map<String, String>> verifyAccount(@RequestParam String email, @RequestParam String otp) {
-        Map<String, String> response;
-        try {
-            response = accountService.verifyAccount(email, otp);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
-        }
-    }
-
-    @PutMapping(value = "/regenerate-otp", produces = "application/json;charset=UTF-8")
-    public ResponseEntity<Map<String, String>> regenerateOtp(@RequestParam String email) {
-        Map<String, String> response;
-        try {
-            response = accountService.regenerateOtp(email);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
-        }
-    }
-
-    @PostMapping(value = "/forget-password", produces = "application/json;charset=UTF-8")
-    public ResponseEntity<Map<String, String>> forgetPassword(@RequestParam String email) {
-        try {
-            Map<String, String> response = accountService.forgetPassword(email);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
-        }
-    }
-
-    @PutMapping(value = "/set-password", produces = "application/json;charset=UTF-8")
-    public ResponseEntity<Map<String, String>> setPassword(@RequestParam String email, @RequestHeader String newPassword) {
-        try {
-            Map<String, String> response = accountService.setPassword(email, newPassword);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
-        }
     }
 }
