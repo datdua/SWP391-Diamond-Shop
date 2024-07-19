f4dc428cefe43d9e8ed35f18aa9984f1c3a41837
Nguyễn Tấn Đạt <toimqn09@gmail.com>
2024-07-19 19:58:43 +0700

    update-dashboard-API-1

--- Front-End/thediamondstore/src/pages/Profile/ProfilePage.js
+++ Front-End/thediamondstore/src/pages/Profile/ProfilePage.js
@@ -193,6 +193,7 @@ const ProfilePage = () => {
         open={openSnackbar}
         autoHideDuration={6000}
         onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
       >
         <Alert onClose={handleCloseSnackbar} severity="success">
           Cập nhật thông tin thành công!
