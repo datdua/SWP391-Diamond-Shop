a2ac636ece1be7f9154eb738194f60a8279df8ef
phuccoder <anhdeptraippt@gmail.com>
2024-07-22 19:43:46 +0700

    Fix UpdateDiamondPriceForm

--- Front-End/thediamondstore/src/api/DiamondPriceAPI.js
+++ Front-End/thediamondstore/src/api/DiamondPriceAPI.js
@@ -63,7 +63,11 @@ export const updateDiamondPrice = async (diamondPriceID, diamondPrice) => {
     );
     return response.data;
   } catch (error) {
-    console.error(error);
+    if (error.response && error.response.data) {
+      throw error.response.data;
+    } else {
+      throw error;
+    }
   }
 };
 
