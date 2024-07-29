0744fedb115301c2d368b1f5feceb4adf6b7dae6
AnPT173 <phanthienan01072003@gmail.com>
2024-07-29 12:19:46 +0700

    fix order detail, create order notification

--- Front-End/thediamondstore/src/components/OrderSidebar/OrderSidebar.js
+++ Front-End/thediamondstore/src/components/OrderSidebar/OrderSidebar.js
@@ -11,8 +11,10 @@ function OrderSidebar({ order, show, onHide }) {
         {orderItem.jewelry && orderItem.jewelry.jewelryName && <p><span className="detail-title">Tên trang sức:</span> <span className="detail-info">{orderItem.jewelry.jewelryName}</span></p>}
         <p><span className="detail-title">Số lượng:</span> <span className="detail-info">{orderItem.quantity}</span></p>
         {orderItem.sizeJewelry && <p><span className="detail-title">Kích thước trang sức:</span> <span className="detail-info">{orderItem.sizeJewelry}</span></p>}
-        <p><span className="detail-title">Giá:</span> <span className="detail-info">{orderItem.price.toLocaleString()}</span></p>
-        <p><span className="detail-title">Giá đã bao gồm phí:</span> <span className="detail-info">{orderItem.totalPrice.toLocaleString()}</span></p>
+        {orderItem.diamond && orderItem.diamond.diamondEntryPrice && <p><span className='detail-title'>Giá:</span> <span className='detail-info'>{orderItem.diamond.diamondEntryPrice.toLocaleString()}</span></p>}
+        {orderItem.jewelry && orderItem.jewelry.jewelryEntryPrice && <p><span className='detail-title'>Giá:</span> <span className='detail-info'>{orderItem.jewelry.jewelryEntryPrice.toLocaleString()}</span></p>}
+        {orderItem.diamond && orderItem.diamond.grossDiamondPrice && <p><span className='detail-title'>Giá đã bao gồm phí:</span> <span className='detail-info'>{orderItem.diamond.grossDiamondPrice.toLocaleString()}</span></p>}
+        {orderItem.jewelry && orderItem.jewelry.grossJewelryPrice && <p><span className='detail-title'>Giá đã bao gồm phí:</span> <span className='detail-info'>{orderItem.jewelry.grossJewelryPrice.toLocaleString()}</span></p>}
       </div>
       <hr />
       <div className="order-section">
@@ -30,14 +32,23 @@ function OrderSidebar({ order, show, onHide }) {
         {orderItem.diamondCertificateImage && (
           <p><span className="detail-title">Chứng nhận kim cương:</span> <br /><a href={orderItem.diamondCertificateImage} target="_blank" rel="noopener noreferrer"><img src={orderItem.diamondCertificateImage} alt="Diamond Certificate" className="certificate-image" /></a></p>
         )}
-        {orderItem.diamondWarrantyImage && (
-          <p><span className="detail-title">Bảo hành kim cương:</span> <br /><a href={orderItem.diamondWarrantyImage} target="_blank" rel="noopener noreferrer"><img src={orderItem.diamondWarrantyImage} alt="Diamond Warranty" className="warranty-image" /></a></p>
+        {orderItem.warranty && orderItem.warranty.diamondID && (
+          <>
+            <p><span className="detail-title">Bảo hành kim cương:</span> <br /><a href={orderItem.warranty.warrantyImage} target="_blank" rel="noopener noreferrer"><img src={orderItem.warranty.warrantyImage} alt="Diamond Warranty" className="warranty-image" /></a></p>
+            <p><span className='detail-title'>Ngày bắt đầu bảo hành: </span><span className='detail-info'>{new Date(orderItem.warrantyHistories[0].effectiveDate).toLocaleDateString()}</span></p>
+            <p><span className="detail-title">Ngày hết hạn bảo hành:</span> <span className="detail-info">{new Date(orderItem.warrantyHistories[0].expirationDate).toLocaleDateString()}</span></p>      
+          </>
         )}
-        {orderItem.jewelryWarrantyImage && (
-          <p><span className="detail-title">Bảo hành trang sức:</span> <br /><a href={orderItem.jewelryWarrantyImage} target="_blank" rel="noopener noreferrer"><img src={orderItem.jewelryWarrantyImage} alt="Jewelry Warranty" className="warranty-image" /></a></p>
+        {orderItem.warranty && orderItem.warranty.jewelryID && (
+          <>
+            <p><span className="detail-title">Bảo hành trang sức:</span> <br /><a href={orderItem.warranty.warrantyImage} target="_blank" rel="noopener noreferrer"><img src={orderItem.warranty.warrantyImage} alt="Jewelry Warranty" className="warranty-image" /></a></p>
+            <p><span className='detail-title'>Ngày bắt đầu bảo hành: </span><span className='detail-info'>{new Date(orderItem.warrantyHistories[0].effectiveDate).toLocaleDateString()}</span></p>
+            <p><span className="detail-title">Ngày hết hạn bảo hành:</span> <span className="detail-info">{new Date(orderItem.warrantyHistories[0].expirationDate).toLocaleDateString()}</span></p>
+            
+          </>
         )}
       </div>
-      <hr/>
+      <hr />
     </>
   );
 
@@ -58,7 +69,18 @@ function OrderSidebar({ order, show, onHide }) {
                 {order[0].account.addressAccount && <p><span className="detail-title">Địa chỉ:</span> <span className="detail-info">{order[0].account.addressAccount}</span></p>}
               </div>
             )}
-            <hr />
+            <hr />           
+            {order[0].order && (
+              <div className="order-section">
+                <h5>Thông tin đơn hàng</h5>
+                {order[0].order.deliveryAddress && <p><span className="detail-title">Địa chỉ giao hàng:</span> <span className="detail-info">{order[0].order.deliveryAddress}</span></p>}
+                {order[0].order.phoneNumber && <p><span className="detail-title">Số điện thoại:</span> <span className="detail-info">{order[0].order.phoneNumber}</span></p>}
+                {order[0].order.startorderDate && <p><span className="detail-title">Ngày đặt hàng:</span> <span className="detail-info">{new Date(order[0].order.startorderDate).toLocaleString()}</span></p>}
+                {order[0].order.deliveryDate && <p><span className="detail-title">Ngày giao hàng dự kiến:</span> <span className="detail-info">{new Date(order[0].order.deliveryDate).toLocaleString()}</span></p>}
+                {order[0].order.transactionNo && <p><span className="detail-title">Mã giao dịch:</span> <span className="detail-info">{order[0].order.transactionNo}</span></p>}        
+              </div>
+            )}
+             <hr />
             <h5 style={{ fontWeight: "bold" }}>Thông tin sản phẩm</h5>
             {order.map((orderItem) => (
               <div key={orderItem.orderDetailID}>
@@ -67,9 +89,17 @@ function OrderSidebar({ order, show, onHide }) {
             ))}
             <div className="order-section">
               <h5>Thông tin tổng đơn đặt hàng</h5>
-              {order[0].promotion && <p><span className="detail-title">Mã khuyến mãi:</span> <span className="detail-info">{order[0].promotion.promotionCode}</span></p>}
-              {order[0].promotion && <p><span className="detail-title">Giá :</span> <span className="detail-info">{order[0].promotion.discountAmount}*{}</span></p>}
-              <p><span className="detail-title">Tổng giá đơn hàng:</span> <span className="detail-info">{order[0].totalPrice.toLocaleString()}</span></p>
+              {order[0].order && (
+                <>
+                {order[0].order.accountPoint != null && <p><span className="detail-title">Điểm sử dụng:</span> <span className='detail-info'>{order[0].order.accountPoint}</span></p>}
+                  {order[0].order.promotionCode && (                    
+                    <p><span className="detail-title">Mã khuyến mãi:</span> <span className="detail-info">{order[0].order.promotionCode}</span></p>
+                  )}
+                  <p><span className="detail-title">Tổng giá trước giảm:</span> <span className="detail-info">{order[0].order.subtotalOrder.toLocaleString()} VND</span></p>
+                  <p><span className="detail-title">Giá đã giảm:</span> <span className="detail-info">- {(order[0].order.subtotalOrder - order[0].order.totalOrder).toLocaleString()} VND</span></p>
+                  <p><span className="detail-title">Tổng giá đơn hàng:</span> <span className="detail-info">{order[0].order.totalOrder.toLocaleString()} VND</span></p>
+                </>
+              )}
             </div>
           </>
         ) : (
